e71d1aa72aacee3c202583d76aeba4b9cc75a22b
CrisSal2 <salgado.chris.m@gmail.com>
2024-05-02 23:18:22 -0700

    added date

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,7 @@
 // Retrieve tasks and nextId from localStorage
 let taskList = JSON.parse(localStorage.getItem("tasks"));
 let nextId = JSON.parse(localStorage.getItem("nextId"));
-const modal = document.querySelector('.modal');
-const backdrop = document.querySelector('.backdrop');
-const addBtn = document.querySelector('.btn');
-const submit = document.querySelector('#submit');
+const submit = document.querySelector('.btn-primary');
 
 
 // Todo: create a function to generate a unique task id
@@ -24,11 +21,68 @@ console.log(generateTaskId());
 
 
 // Todo: create a function to create a task card
+
 function createTaskCard(task) {
+  const mainEl = document.querySelector("#todo-cards");
+  const task = document.createElement("div");
+  const title = document.createElement("h1");
+  const date = document.createElement("h2");
+  const todo = document.createElement("p");
+
+  title.textContent = task.title;
+  date.textContent = task.date;
+  todo.textContent = task.content;
+
+  mainEl.append(task);
+  task.append(h1El, h2El, paraEl);
+
+  task.setAttribute('class', 'card');
+
+}
+
+
+
+/* {
+const tasks = JSON.parse(localStorage.getItem("tasks"));
+
+const mainEl = document.querySelector("#todo-cards");
+
+for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+
+    const divEl = document.createElement("div");
+    const pEl = document.createElement("p");
+    const h2El = document.createElement("h2");
+    const h3El = document.createElement("h3");
+
+    h3El.textContent = task.date;
+    h2El.textContent = task.title;
+    pEl.textContent = task.content;
+
+    mainEl.append(divEl);
+    divEl.append(h2El, h3El, pEl);
+
+    divEl.setAttribute('class', 'card');
+
+}
+} */
+/* function createTaskCard(task) {
     const taskCard = document.createElement("div");
     taskCard.classList.add("card");
 
 }
+
+$( function() {
+  $( "#sortable" ).sortable({
+    revert: true
+  });
+  $( "#draggable" ).draggable({
+    connectToSortable: "#sortable",
+    helper: "clone",
+    revert: "invalid"
+  });
+  $( "ul, li" ).disableSelection();
+} ); */
 
 // Todo: create a function to render the task list and make cards draggable
 function renderTaskList() {
@@ -36,22 +90,12 @@ function renderTaskList() {
 }
 
 // Todo: create a function to handle adding a new task
-function openModal(event) {
-  event.preventDefault();
-
-  backdrop.style.display = 'block';
-  modal.style.display = 'block';
-
-}
-addBtn.addEventListener('click', openModal);
-
-
 function handleAddTask(event){
     event.preventDefault();
 
     const tasks = JSON.parse(localStorage.getItem('tasks')) || [];
     const titleInput = document.querySelector('#title-of-task');
-    const dateInput = document.querySelector('#date-text');
+    const dateInput = document.querySelector('#datepicker');
     const taskInput = document.querySelector('#task-text');
 
     tasks.push({
@@ -61,11 +105,9 @@ function handleAddTask(event){
       task: taskInput.value,
 
     });
-
-    backdrop.style.display = 'none';
-    modal.style.display = 'none';
-
     localStorage.setItem('tasks', JSON.stringify(tasks));
+
+    createTaskCard();
 }
 
 submit.addEventListener('click', handleAddTask);
@@ -83,4 +125,4 @@ function handleDrop(event, ui) {
 // Todo: when the page loads, render the task list, add event listeners, make lanes droppable, and make the due date field a date picker
 $(document).ready(function () {
 
-});
+})
